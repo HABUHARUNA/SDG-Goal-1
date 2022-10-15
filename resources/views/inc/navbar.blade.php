dd3e84480d9497029cf669356b74fb95eaf6a8b5
HABUHARUNA <habuhope68@gmail.com>
2022-10-15 11:06:26 +0100

    first commit

---
+++ resources/views/inc/navbar.blade.php
@@ -0,0 +1,46 @@
+<nav class="navbar navbar-expand-lg bg-dark sticky-top navbar-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand mx-5" href="/"><h1> <abbr title="Skill For Wealth">SFW</abbr> </h1></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse d-flex" id="navbarNavAltMarkup">
+        
+         <div class="nav-item mx-auto">
+            <ul class="navbar-nav">
+                <li class="nav-item me-5">
+                    <a class="nav-link active" aria-current="page" href="/">Home</a>
+                </li>
+                <li class="nav-item me-5">
+                    <a class="nav-link" href="/about">About Us</a>
+                </li>
+                <li class="nav-item me-5">
+                    <a class="nav-link" href="/contact">Contact</a>
+                </li>
+                <li class="nav-item me-5">
+                    <a class="nav-link" href="/support">Support Us</a>
+                </li>
+            </ul>
+        </div>
+        <div class=" d-flex p-2">
+            <ul class="navbar-nav">
+                <div class="dropdown">
+                    <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                     Register
+                    </a>
+                  
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                      <li><a class="dropdown-item" href="#">Volunteer</a></li>
+                      <li><a class="dropdown-item" href="#"></a></li>
+                    </ul>
+                  </div> 
+                    <li class="nav-item mx-5">
+                        <a class="nav-link" href="#">login</a>
+                    </li>
+                
+           </ul>
+        </div>
+          
+      </div>
+    </div>
+  </nav>
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+use App\Http\Contrrollers\VolunteerController;
+use App\Http\Controllers\StudentController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/', [PagesController::class, 'home']);
+Route::get('/about', [PagesController::class, 'about']);
+Route::get('/contact', [PagesController::class, 'contact']);
+Route::get('/support', [PagesController::class, 'support']);
